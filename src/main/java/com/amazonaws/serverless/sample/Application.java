ccb593dd4c85d8bc4c7997d2ff5e5037c3d3556c
Pavan <103222512+PavanIncedo@users.noreply.github.com>
2022-05-03 10:37:22 -0400

    Add files via upload

---
+++ src/main/java/com/amazonaws/serverless/sample/Application.java
@@ -0,0 +1,19 @@
+package com.amazonaws.serverless.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import com.amazonaws.serverless.sample.controller.PingController;
+
+
+@SpringBootApplication
+// We use direct @Import instead of @ComponentScan to speed up cold starts
+// @ComponentScan(basePackages = "com.amazonaws.serverless.sample.controller")
+@Import({ PingController.class })
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
